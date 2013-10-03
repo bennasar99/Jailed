db945f5cb333db6dd0e74a5a8c756d1c7b56ef0a
tonibm19 <tonibm19@gmail.com>
2013-10-03 11:13:10 +0200

    Added hooks, v1 finished.

---
+++ hooks.lua
@@ -0,0 +1,52 @@
+function OnPlayerBreakingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, BlockType, BlockMeta)
+      local UsersIni = cIniFile("users.ini")
+      local SettingsIni = cIniFile( "Plugins/Jailed/settings.ini" )
+      UsersIni:ReadFile()
+      SettingsIni:ReadFile()
+      if (UsersIni:GetValue(Player:GetName(),   "Jailed") == "true") and (SettingsIni:GetValue("Enabled",   "Dig") == "false") then 
+             Player:SendMessage(cChatColor.Red .. "You are jailed")
+             return true
+      else
+             return false
+      end
+end
+
+function OnPlayerPlacingBlock(Player, BlockX, BlockY, BlockZ, BlockFace, CursorX, CursorY, CursorZ, BlockType)
+      local UsersIni = cIniFile("users.ini")
+      local SettingsIni = cIniFile( "Plugins/Jailed/settings.ini" )
+      UsersIni:ReadFile()
+      SettingsIni:ReadFile()
+      if (UsersIni:GetValue(Player:GetName(),   "Jailed") == "true") and (SettingsIni:GetValue("Enabled",   "Place") == "false") then 
+             Player:SendMessage(cChatColor.Red .. "You are jailed")
+             return true
+      else 
+             return false
+      end
+end
+
+function OnExecuteCommand(Player, CommandSplit)
+      print(CommandSplit)
+      local UsersIni = cIniFile("users.ini")
+      local SettingsIni = cIniFile( "Plugins/Jailed/settings.ini" )
+      UsersIni:ReadFile()
+      SettingsIni:ReadFile()
+      if (UsersIni:GetValue(Player:GetName(),   "Jailed") == "true") and (SettingsIni:GetValue("Enabled",   "Commands") == "false") then
+             Player:SendMessage(cChatColor.Red .. "You are jailed") 
+             return true
+      else 
+             return false
+      end
+end
+
+function OnChat(Player, Message)
+      local UsersIni = cIniFile("users.ini")
+      local SettingsIni = cIniFile( "Plugins/Jailed/settings.ini" )
+      UsersIni:ReadFile()
+      SettingsIni:ReadFile()
+      if (UsersIni:GetValue(Player:GetName(),   "Jailed") == "true") and (SettingsIni:GetValue("Enabled",   "Chat") == "false") then 
+             Player:SendMessage(cChatColor.Red .. "You are jailed")
+             return true
+      else 
+             return false
+      end
+end
